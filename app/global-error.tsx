1211bcaba915a46a41a2955ae93851fea69c4511
sarojswainepam <saroj_swain@epam.com>
2025-04-30 12:58:29 +0530

    Global error and Route groups

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -16,7 +16,7 @@ export default function GlobalError({
  
   return (
     <div>
-      <h2>Something went wrong! Global Error</h2>
+      <h2 className='text-amber-600'>Something went wrong! Global Error</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
